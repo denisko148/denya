056817d45173c66fe11d98acdcce83fa8981f234
Michail <mikeke@bk.ru>
2017-01-17 10:22:30 +0200

    Final version

---
+++ Mike.java
@@ -0,0 +1,34 @@
+public class Mike
+{
+	int m, n, o;
+		double z1 = 0;
+			double z2 = 0;
+				double z3 = 0;
+					
+							public Mike(int m, int n, int o)
+									{
+												this.m = m;
+															this.n = n;
+																		this.o = o;
+																				}
+																						public void result (int m, int n, int o)
+																								{
+																											z1 = m + n + o;
+																														z2 = z1 * 4;
+																																	z3 = z2 / 3;
+																																				System.out.printf ("%.2f\n", z3);
+																																						}
+																																								public void res1 ()
+																																										{
+																																													System.out.printf ("%.2f\n", z1);
+																																															}
+																																																	public void res2 ()
+																																																			{
+																																																						System.out.printf ("%.2f\n", z2);
+																																																								}
+																																																										public void res3 ()
+																																																												{
+																																																															System.out.printf ("%.2f\n", z3);
+																																																																	}
+																																																																	}
+																																																																		
--- program.java
+++ program.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Program
+public class program
 {
 	Scanner sc = new Scanner (System.in);
 	String start = "";
@@ -9,7 +9,7 @@ public class Program
 	public static void main (String[] args)
 	{
 		System.out.println ("Hello");
-		Program program = new Program();
+		program program = new program();
 		program.start();
 	}
 	public void start ()
@@ -68,37 +68,4 @@ public class Program
 	}
 }
 
-public class Mike
-{
-	int m, n, o;
-	double z1 = 0;
-	double z2 = 0;
-	double z3 = 0;
-	
-		public Mike(int m, int n, int o)
-		{
-			this.m = m;
-			this.n = n;
-			this.o = o;
-		}
-		public void result (int m, int n, int o)
-		{
-			z1 = m + n + o;
-			z2 = z1 * 4;
-			z3 = z2 / 3;
-			System.out.printf ("%.2f\n", z3);
-		}
-		public void res1 ()
-		{
-			System.out.printf ("%.2f\n", z1);
-		}
-		public void res2 ()
-		{
-			System.out.printf ("%.2f\n", z2);
-		}
-		public void res3 ()
-		{
-			System.out.printf ("%.2f\n", z3);
-		}
-}
-	
+
